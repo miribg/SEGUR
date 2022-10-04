09e453c434e176ddd8ab3d197d7470d5901bd2b9
miribg <62209102+miribg@users.noreply.github.com>
2022-10-04 23:04:53 +0200

    Add files via upload

---
+++ irudia.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+hash="e5ed313192776744b9b93b1320b5e268"
+
+for f in irudia/*.jpg;do
+	var=$(sudo md5sum $f | cut -d ' ' -f 1)
+	if [[ $var == $hash ]]
+	then
+		echo "$f"		
+	fi
+done
